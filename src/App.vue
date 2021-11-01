0cc4c41d91587f3d18543a3c0d11b83a8d06bbc3
Crowrvo <crowrvoblackfeather@gmail.com>
2021-11-01 17:11:06 -0300

    git commit -m "first commit"

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="center">
+    <img src="./assets/Logo.png" />
+    <span class="title">em breve</span>
+    <span class="subtitle">Vamos mudar a sua forma de ler</span>
+    <span class="aguarde">AGUARDE!</span>
+  </div>
+  <div class="cover"></div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
-</script>
-
 <style lang="scss">
-#app {
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap");
+@import "./assets/scss/responsividade.scss";
+
+body {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  align-items: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 100%);
+  background-color: #222025;
+  color: #fff;
+  margin: 0;
+  overflow: hidden;
+  padding: 0%;
+}
+.center {
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: 1fr;
+  align-items: center;
+  vertical-align: middle;
+  img {
+    margin: auto;
+    height: 25vh;
+    margin-bottom: 12vh;
+  }
+  .title {
+    font-family: "Bebas Neue";
+    text-align: center;
+    font-size: max(16.5vw, 16.5vh);
+    line-height: max(calc(14.5vw - 3.75vw), calc(16.5vh - 3.75vh));
+  }
+  .subtitle {
+    font-family: "Roboto Condensed";
+    text-align: center;
+    font-size: max(3.75vw, 3.75vh);
+    line-height: max(3.75vw, 3.75vh);
+  }
+}
+.aguarde {
+  font-family: "Bebas Neue";
+  font-style: normal;
+  font-weight: normal;
+  margin-top: 4vh;
+  font-size: max(5.75vw, 6.75vh);
+  line-height: max(5.75vw, 6.75vh);
+  text-align: center;
+}
+.cover {
+  top: 0;
+  left: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 99;
+  display: block;
+  background-color: transparent;
 }
 </style>
+
